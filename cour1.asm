932fedc5ef2d697bcd76041e3552309ad35bc02b
PicoisCharlotte <charlotte.picois.sio@gmail.com>
2019-06-04 16:10:37 +0200

    first commit

---
+++ cour1.asm
@@ -0,0 +1,8 @@
+#addi//additionne un registre avec un nombre et le met dans un registre
+
+li t0, 3 #Attribu la valeur 3 au registre t0
+li t1, 4 #Attribu la valeur 4 au registre t1
+
+add a0, t0, t1 #Additionne t0 et t1 et met le résultat dans le registre a0
+
+li a7, 1 #Attribu la valeur 1 au registre a7
