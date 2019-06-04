43844bf62669d66e9087fbfe559efbaa4972ded4
David <dlinhares@reseau-ges.fr>
2019-06-04 12:59:50 +0200

    :tada: [Add] add exo

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-int main(void) {
-    // Le descripteur de fichier 0 indique la stdout
-    const char my_greetings[] = "Hello World";
-    write(1, my_greetings, sizeof(my_greetings));
-    return 0;
-}
--- hello.js
+++
@@ -1,4 +0,0 @@
-// A titre de comparaison voici la même façon de faire en JavaScript (nodeJS)
-
-const my_greetings = 'Hello World';
-console.log(my_greetings);
