156e14439752843030b5677bab5df2e457438206
Thibaut <tibdev78@gmail.com>
2019-06-08 15:14:40 +0200

    push largest 2

--- Exercices/largest.asm
+++ Exercices/largest.asm
@@ -1,6 +1,6 @@
 .data
 #declaration array
-array: .word 5,11,2,12
+array: .word 5,11,2,1
 #size array
 size: .word 4
 #largest
@@ -12,24 +12,28 @@ la t0, array
 la t1, size
 la t2 largest
 
-mv t2, a0 #copie valeur largest dans a0 pour l'afficher à la fin
-
-lw t3, 0(t1) #on recupere la valeure de size par l'adresse
+lw t3, 0(t1) #on recupere la valeur de size par l'adresse
+lw t4, 0(t2) #on recupere la valeur de largest par l'adresse
 
 slli t3, t3, 2 #set l'adresse de size sur 32 bit
-
 add t5, t0, t3 #déterminer l'adresse de fin du tableau
 
 for:
-beq t0, t5, end_for # i < size ? cursor = fin 
-lw s2, 0(t0) # array[i] dans le registre s2
-blt a0, s2, if #if largest < array[i], on va dans le if
-if: 
- mv a0, s2 #largest = array[i]
-end_if:
-j for #continue
+sub s1, t5, t0 #i--
+beq s1, zero, end_for # i < size
+lw s2, 0(t0) #array[i]
+blt t4, s2, mv_max #i largest < array[i]
+addi t0, t0, 4 #decale le curseur
+j for
 end_for:
+j end
 
+mv_max:
+mv t4, s2 #largest = array[i]
+j for
+
+end:
+mv a0, t4 #copie valeur largest dans a0 pour l'afficher à la fin
 li a7, 1 #print largest
 
 ecall
