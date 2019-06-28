ca81a1454a48b7ac4671fc292634a52a93041b51
PicoisCharlotte <charlotte.picois.sio@gmail.com>
2019-06-28 09:49:47 +0200

    exo parcourir tableau + exo find max

---
+++ findMax.asm
@@ -0,0 +1,49 @@
+.data
+
+array: .word 1,4,12,6 #initialise un tableau avec des valeurs
+
+size: .word 4
+
+max: .word -1000 
+
+
+.text
+
+la s2, array #on met array dans le registre s0
+la s3, size #on met la taille dans le registre s3
+la s4, max #on met le nombre a rechercher dans le registre s4
+
+lw t4, 0(s3) #on recupere la taille
+lw t5, 0(s4) #on recupere le nombre
+
+li a0, 0 #on initialise
+
+slli t4, t4, 2
+
+add t6, s2, t4
+
+for:
+	sub t4, t6, s2 #soustraction de deux registre pour vérifié si on est a la fin du tableau
+	beq t4, zero, end_for
+	lw s1, 0(s2) #on recupere la valeur de array[i]
+	
+	bgt s1, t5, find_max  #condition if 
+	
+	addi s2, s2, 4 
+	
+	j for
+	
+end_for:
+	j end
+	
+find_max:
+	mv t5, s1
+	j for
+	
+	
+end:
+mv a0, t5
+li a7, 1
+ecall
+
+
---
+++ parcourirArray.asm
@@ -0,0 +1,47 @@
+.data
+
+array: .word 1,4,12,6,18 #initialise un tableau avec des valeurs
+
+size: .word 5
+
+number: .word 12 #on cherche le nombre 12
+
+
+.text
+
+la s2, array #on met array dans le registre s0
+la s3, size #on met la taille dans le registre s3
+la s4, number #on met le nombre a rechercher dans le registre s4
+
+lw t4, 0(s3) #on recupere la taille
+lw t5, 0(s4) #on recupere le nombre
+
+li a0, 0 #on initialise
+
+slli t4, t4, 2
+
+add t6, s2, t4
+
+for:
+	sub t4, t6, s2 #soustraction de deux registre pour vérifié si on est a la fin du tableau
+	lw s1, 0(s2) #on recupere la valeur de array[i]
+	
+	beq t5, s1, copie_value #condition if 
+	
+	beq t4, zero, end_for 
+	
+	addi s2, s2, 4 
+	
+	j for
+	
+end_for:
+	j end
+	
+copie_value:
+	mv a0, t5
+	
+end:
+li a7, 1
+ecall
+
+
