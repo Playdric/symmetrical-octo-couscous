db726c27a0e1c6900b034f5ec652c0ec4d2b1ac1
PicoisCharlotte <charlotte.picois.sio@gmail.com>
2019-06-28 10:47:32 +0200

    add comments

--- findMax.asm
+++ findMax.asm
@@ -11,10 +11,10 @@ max: .word -1000
 
 la s2, array #on met array dans le registre s0
 la s3, size #on met la taille dans le registre s3
-la s4, max #on met le nombre a rechercher dans le registre s4
+la s4, max #on met le max dans le registre s4
 
 lw t4, 0(s3) #on recupere la taille
-lw t5, 0(s4) #on recupere le nombre
+lw t5, 0(s4) #on recupere le max
 
 li a0, 0 #on initialise
 
@@ -24,10 +24,10 @@ add t6, s2, t4
 
 for:
 	sub t4, t6, s2 #soustraction de deux registre pour vérifié si on est a la fin du tableau
-	beq t4, zero, end_for
+	beq t4, zero, end_for #si on a finit de parcourir le tableau on quitte la boucle
 	lw s1, 0(s2) #on recupere la valeur de array[i]
 	
-	bgt s1, t5, find_max  #condition if 
+	bgt s1, t5, find_max  #condition if greater (if array[i] > max -> on appel find_max)
 	
 	addi s2, s2, 4 
 	
@@ -37,7 +37,7 @@ end_for:
 	j end
 	
 find_max:
-	mv t5, s1
+	mv t5, s1 #copie la valeur de t1 (array[i]) dans t5 (max)
 	j for
 	
 	
--- parcourirArray.asm
+++ parcourirArray.asm
@@ -20,25 +20,25 @@ li a0, 0 #on initialise
 
 slli t4, t4, 2
 
-add t6, s2, t4
+add t6, s2, t4 
 
 for:
 	sub t4, t6, s2 #soustraction de deux registre pour vérifié si on est a la fin du tableau
 	lw s1, 0(s2) #on recupere la valeur de array[i]
 	
-	beq t5, s1, copie_value #condition if 
+	beq t5, s1, copie_value #condition if ==
 	
-	beq t4, zero, end_for 
+	beq t4, zero, end_for #si on a finit de parcourir le tableau on quitte la boucle
 	
 	addi s2, s2, 4 
 	
 	j for
 	
 end_for:
-	j end
+	j end #quitte la boucle
 	
 copie_value:
-	mv a0, t5
+	mv a0, t5 #copie la value de a0 dans le registre t5
 	
 end:
 li a7, 1
